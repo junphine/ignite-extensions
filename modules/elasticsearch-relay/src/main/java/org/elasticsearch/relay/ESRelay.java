cadcb850664347b5dbe4d3abcc6de8726b9c6c71
junphine <junphine@126.com>
2026-04-23 17:59:34 +0800

    remove unused xml config file

--- modules/elasticsearch-relay/src/main/java/org/elasticsearch/relay/ESRelay.java
+++ modules/elasticsearch-relay/src/main/java/org/elasticsearch/relay/ESRelay.java
@@ -36,11 +36,11 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 /**
